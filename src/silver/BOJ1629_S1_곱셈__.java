2aecfc63474306f79f953e82159658045d9e0bc5
QQQomputer <3477qq@gmail.com>
2024-08-07 00:43:53 +0900

    not yet

---
+++ src/silver/BOJ1629_S1_곱셈__.java
@@ -0,0 +1,10 @@
+package silver;
+
+public class BOJ1629_S1_곱셈__ {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/silver/BOJ1740_S4_거듭제곱__.java
@@ -0,0 +1,18 @@
+package silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ1740_S4_거듭제곱__ {
+	
+	//n ≤ 500,000,000,000
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long n = Integer.parseInt(br.readLine());
+
+		
+		
+	}
+
+}
